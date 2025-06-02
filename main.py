64e574c6bdb49d1ffb47da39828da9d07ed3c622
ericdelamata <2023_eric.de@iticbcn.cat>
2025-06-02 16:54:37 +0200

    exercici2

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from Services.users import insert_user
 
 @app.post("users/insert/", response_model=dict)
 async def insertUser(name, surname, email, description, course, year, direction, CP, Password):
-    insert_user(name, surname, email, description, course, year, direction, CP, Password)
+    correct = insert_user(name, surname, email, description, course, year, direction, CP, Password)
     
-    return {"correcte":True}
+    return {"correcte":correct}
