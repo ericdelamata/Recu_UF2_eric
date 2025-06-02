787b4c588ceb780d18e02effec530e64e6f013e9
ericdelamata <2023_eric.de@iticbcn.cat>
2025-06-02 17:14:12 +0200

    exercici3

--- Services/users.py
+++ Services/users.py
@@ -1,17 +1,15 @@
 import psycopg2
 from ..config.connect import connect
 
-def insert_user(name, surname, email, description, course, year, direction, CP, Password):
+def get_user(name):
     conn = connect()
     cursor = conn.cursor()
     try:
-        cursor.execute('''INSERT INTO "user"."user"(
-	    name, surname, email, description, course, year, direction, "CP", password)
-	    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);''', (name, surname, email, description, course, year, direction, CP, Password))
+        cursor.execute('''SELECT name, surname, email, description, course, year FROM "user"."user" WHERE name=%s;''', (name))
         
+        user = cursor.fetchone()
         
-        print("inserted correctly")
-        return True
+        return user
     except(Exception, psycopg2.Error) as error:
         print("Error: ", error)
-        return False
+        return {"Error": error}
